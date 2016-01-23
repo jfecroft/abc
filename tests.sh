05418dcf17d429954197f62e39f4a16051256402
jfecroft <j.f.e.croft@gmail.com>
2016-01-22 16:20:39 -0800

    imprive tests - print messages on results

--- tests.sh
+++ tests.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 make abc
 
+compare ()
+{
+if cmp $1 $2 >/dev/null 2>&1
+then
+    echo "pass $1 and $2 are the same"
+else
+    echo "fail $1 and $2 differ"
+fi
+}
+
 time ./abc.x < fhd.d > fhd.new  
-diff fhd.new fhd.out
+compare fhd.new fhd.out
 
 time ./abc.x < clhd.d > clhd.new
-diff clhd.new clhd.out
+compare lhd.new clhd.out
 
 time ./abc.x < hd2.d > hd2.new
-diff hd2.new hd2.out
+compare hd2.new hd2.out
 
